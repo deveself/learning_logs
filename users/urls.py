c06f89ff0ae506ea33b9538bd416bef66e06fe46
deveself <deveself@163.com>
2020-10-17 18:56:48 +0800

    learning_logs completed

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+"""为应用程序users定义URL模式"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    url(r'^login/$',LoginView.as_view(template_name='users/login.html'),name='login'),
+    url(r'^logout/$',views.logout_view,name='logout'),
+    url(r'^register/$',views.register,name='register'),
+    
+]
+
+
+
